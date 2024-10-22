62ea0d399234494d4d4b6ee2c1bc08fb1d637e8a
David25adm <165786937+David25adm@users.noreply.github.com>
2024-10-22 20:40:27 -0300

    Circulo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function alterarCirculo(){
+function alterarCirculo() {
     const Circulo = document.getElementById('Circulo');
-}
+
 
 const corAleatoria = '#'+
 Math.floor(Math.random()*16777215).toString(16);
@@ -10,3 +10,4 @@ const novoTamanho =Math.floor(Math.random()* (200-50+1)) + 50;
 Circulo.style.backgroudColor = corAleatoria
 Circulo.style.width = novoTamanho + 'px'
 Circulo.style.height = novoTamanho + 'px'
+}
